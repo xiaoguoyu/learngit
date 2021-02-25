db01ae4228d08c3037cec06fc4d530d5fa141b68
xiaoguoyu <xiao_guoyu@126.com>
2021-02-25 09:53:44 +0800

    在chaos里面使用了栈，是代码更简洁紧凑，且更加快速

--- chaos_programming_languege.c
+++ chaos_programming_languege.c
@@ -39,7 +39,8 @@
 1 -> 0
 
 
-[]中的指令会一直循环直到指针指向的数的值为0就会退出但只退出一层循环，不会退出所有循环，相当于break
+[]中的指令会一直循环直到指针指向的数的值为0就会退出但只退出一层循环，不会退出所有循环，相当于break，
+因为退出一次后会有一次“无敌时间”也就是这一次会忽略（不然就一下子退出了所有的循环了）
 
 
 .会输出一个ASCII编码为当前指针指向的数的值的字符（有点绕口哈）
@@ -79,86 +80,40 @@ hello, world
 #include<stdio.h>
 #define MAX 5000
 
-int loop(char commend[], int* pointer, int* map)
-{
-    if (*(map + *pointer) == 0)
-    {
-        return 1;
-    }
-    int num = 0;
-    int isOK = 0;
-    while (commend[num] != '\0')
-    {
-        
-        switch (commend[num])
-        {
-            case '<':{
-                (*pointer)--;
-                break;
-            }
-            case '>':{
-                (*pointer)++;
-                break;
-            }
-            case '+':{
-                (*(map + *pointer))++;
-                break;
-            }
-            case '-':{
-                (*(map + *pointer))--;
-                break;
-            }
-            case '.':{
-                printf("%c", *(map + *pointer));
-                break;
-            }
-            case '[':{
-                char new_commend[MAX];
-                int aboutthisnameihavenoidea = 0;
-                for (int the_num = num+1;; the_num++)
-                {
-                    if(commend[the_num] == ']'){
-                        new_commend[aboutthisnameihavenoidea] = '\0';
-                        while(loop(new_commend, pointer, map) == 0)
-                            ;
-                        num += aboutthisnameihavenoidea;
-                        num += 1;
-                        break;
-                    }else{
-                        new_commend[aboutthisnameihavenoidea] = commend[the_num];
-                        aboutthisnameihavenoidea++;
-                    }
-                }
-                break;
-            }
-            case '?':{
-                printf("%d -> %d\n", *pointer, *(map + *pointer));
-                break;
-            }
-        }
-        num++;
-        if (*(map + *pointer) == 0)
-        {
-            isOK = 1;
-            break;
-        }
-    }
-    return isOK;
+void push(int* stack, int num){
+    int i = 0;
+    while (*(stack+i) != 0)
+        i++;
+    *(stack+i) = num;
+}
+
+int pop(int* stack, int mod){
+    int i = 0;
+    while (*(stack+i) != 0)
+        i++;
+    return *(stack+--i);
+    if(mod)
+        *(stack+i) = 0;
 }
 
 int main(){
     int map[1000];
+    int loopstack[100];
     for (int i = 0; i < 1000; i++)
     {
         map[i] = 0;
     }
-    int old_pointer = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        loopstack[i] = 0;
+    }
+    int pointer = 0;
+    int islooping = 0;
     while (1)
     {
         char commend[MAX];
         printf("::");
         scanf("%s", commend);
-        int* pointer = &old_pointer;
         int num = 0;
         if (commend[0] == 'f')
         {
@@ -177,104 +132,105 @@ int main(){
                 switch (commend[num])
                 {
                     case '<':{
-                        (*pointer)--;
+                        pointer--;
                         break;
                     }
                     case '>':{
-                        (*pointer)++;
+                        pointer++;
                         break;
                     }
                     case '+':{
-                        (*(map + *pointer))++;
+                        map[pointer]++;
                         break;
                     }
                     case '-':{
-                        (*(map + *pointer))--;
+                        map[pointer]--;
                         break;
                     }
                     case '.':{
-                        printf("%c", *(map + *pointer));
+                        printf("%c", map[pointer]);
                         break;
                     }
                     case '[':{
-                        char new_commend[MAX];
-                        int aboutthisnameihavenoidea = 0;
-                        for (int the_num = num+1;; the_num++)
-                        {
-                            if(commend[the_num] == ']'){
-                                new_commend[aboutthisnameihavenoidea] = '\0';
-                                while(loop(new_commend, pointer, map) == 0)
-                                    ;
-                                num += aboutthisnameihavenoidea;
-                                num += 1;
-                                break;
-                            }else{
-                                new_commend[aboutthisnameihavenoidea] = commend[the_num];
-                                aboutthisnameihavenoidea++;
-                            }
-                        }
+                        push(loopstack, num);
+                        islooping++;
+                        break;
+                    }
+                    case ']':{
+                        num = pop(loopstack, 0);
                         break;
                     }
                     case '?':{
-                        printf("%d -> %d\n", *pointer, *(map + *pointer));
+                        printf("%d -> %d\n", pointer, map[pointer]);
                         break;
                     }
                 }
+                if ((map[pointer] == 0) && (islooping))
+                {
+                    int i = 1;
+                    while (commend[num+i] != ']')
+                    {
+                        i++;
+                    }
+                    num+=i;
+                    pop(loopstack, 1);
+                    islooping--;
+                }
                 num++;
             }
-        }
-        else
+        }else{
             while (commend[num] != '\0')
             {
                 switch (commend[num])
                 {
                     case '<':{
-                        (*pointer)--;
+                        pointer--;
                         break;
                     }
                     case '>':{
-                        (*pointer)++;
+                        pointer++;
                         break;
                     }
                     case '+':{
-                        (*(map + *pointer))++;
+                        map[pointer]++;
                         break;
                     }
                     case '-':{
-                        (*(map + *pointer))--;
+                        map[pointer]--;
                         break;
                     }
                     case '.':{
-                        printf("%c", *(map + *pointer));
+                        printf("%c", map[pointer]);
                         break;
                     }
                     case '[':{
-                        char new_commend[MAX];
-                        int aboutthisnameihavenoidea = 0;
-                        for (int the_num = num+1;; the_num++)
-                        {
-                            if(commend[the_num] == ']'){
-                                new_commend[aboutthisnameihavenoidea] = '\0';
-                                while(loop(new_commend, pointer, map) == 0)
-                                    ;
-                                num += aboutthisnameihavenoidea;
-                                num += 1;
-                                break;
-                            }else{
-                                new_commend[aboutthisnameihavenoidea] = commend[the_num];
-                                aboutthisnameihavenoidea++;
-                            }
-                        }
+                        push(loopstack, num);
+                        islooping++;
+                        break;
+                    }
+                    case ']':{
+                        num = pop(loopstack, 0);
                         break;
                     }
                     case '?':{
-                        printf("%d -> %d\n", *pointer, *(map + *pointer));
+                        printf("%d -> %d\n", pointer, map[pointer]);
                         break;
                     }
                 }
+                if ((map[pointer] == 0) && (islooping))
+                {
+                    int i = 1;
+                    while (commend[num+i] != ']')
+                    {
+                        i++;
+                    }
+                    num+=i;
+                    pop(loopstack, 1);
+                    islooping--;
+                }
                 num++;
             }
+        }
     }
-        //loop(commend, &pointer, map);
     return 0;
 }
